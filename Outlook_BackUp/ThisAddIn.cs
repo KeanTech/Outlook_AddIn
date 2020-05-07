5d2d5628c5c72490207c78f0293113ee13b358b9
Kean <50844146+Mojiez@users.noreply.github.com>
2020-05-07 08:53:03 +0200

    made a new test

--- Outlook_BackUp/ThisAddIn.cs
+++ Outlook_BackUp/ThisAddIn.cs
@@ -13,9 +13,10 @@ namespace Outlook_BackUp
     {
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+            
             Outlook.MAPIFolder inbox = this.Application.ActiveExplorer().Session.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
-            Outlook.Items unreadItems = inbox.Items.Restrict("[Unread]=true");
-            MessageBox.Show(string.Format("Unread items in inbox = {0}", unreadItems.Count));
+            Outlook.Items unreadItems = inbox.Items;
+            MessageBox.Show(string.Format("A backup was made on the selected path"));
         }
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
